528c08ac8ee3406de50f9d9445397c96438b8258
lotem.kitaroo <lotemkitaroo@gmail.com>
2026-02-02 18:44:21 +0200

    vite url

--- frontend/src/api/apiPrivate.ts
+++ frontend/src/api/apiPrivate.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 export const apiPrivate = axios.create({
-  baseURL: import.meta.env.BASE_URL || "/api/v1",
+  baseURL: import.meta.env.VITE_API_URL || "/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
