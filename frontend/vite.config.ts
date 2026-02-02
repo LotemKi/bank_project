82809e29403c7c292dc28a1200db08a35d6d1347
lotem.kitaroo <lotemkitaroo@gmail.com>
2026-02-02 18:21:21 +0200

    base url

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,16 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
     server: {
-        proxy: {
+        proxy: process.env.NODE_ENV === 'development' ? {
             '/api': {
                 target: 'http://localhost:3000',
                 changeOrigin: true,
                 secure: false
-            },
-        },
+            }
+        } : undefined
     },
 });
