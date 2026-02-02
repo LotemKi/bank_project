3418bd45955fa58f46b9e7302ce40a03c9a55a74
lotem.kitaroo <lotemkitaroo@gmail.com>
2026-02-02 16:23:17 +0200

    app cors add

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import cors from 'cors';               // <-- הוספה
+import cors from 'cors';
 import authRoutes from './routes/auth.routes.js';
 import transactionRoutes from './routes/transaction.routes.js';
 
