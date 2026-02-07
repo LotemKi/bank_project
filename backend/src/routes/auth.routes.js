e45b3e60e703b0b74261c588e3696fe7b59eb8fc
lotem.kitaroo <lotemkitaroo@gmail.com>
2026-02-07 16:08:35 +0200

    server

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -4,10 +4,10 @@ import authMiddleware from '../middleware/auth.middleware.js';
 
 const router = express.Router();
 
-router.post('/signup', controller.signup);
-router.get('/verify', controller.verify);
-router.post('/login', controller.login);
-router.post('/logout', authMiddleware, controller.logout);
+router.post('/auth/signup', controller.signup);
+router.get('/auth/verify', controller.verify);
+router.post('/auth/login', controller.login);
+router.post('/auth/logout', authMiddleware, controller.logout);
 router.get('/me', authMiddleware, controller.me);
 
 export default router;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,38 +1,8 @@
 import 'dotenv/config';
-import http from 'http';
-import { Server } from 'socket.io';
 import app from './app.js';
 import connectDB from './services/db.service.js';
 
-const PORT = process.env.PORT || 5000;
-
+const PORT = process.env.PORT;
 await connectDB();
 
-const server = http.createServer(app);
-
-// Initialize Socket.IO
-export const io = new Server(server, {
-    cors: {
-        origin: '*',
-        methods: ['GET', 'POST']
-    }
-});
-
-io.on('connection', (socket) => {
-    console.log('Client connected:', socket.id);
-
-    socket.on('chatMessage', (msg) => {
-        console.log('Received from client:', msg);
-
-        const botReply = `Bot says: ${msg.split('').reverse().join('')}`;
-
-        socket.emit('botMessage', botReply);
-    });
-
-    socket.on('disconnect', () => {
-        console.log('Client disconnected:', socket.id);
-    });
-});
-
-// Start server
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`)); 
